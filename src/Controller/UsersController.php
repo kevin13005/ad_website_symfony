1276eeb0c40d92595443185cf71406b60d90d01e
kevin <kevin.prevost69@gmail.com>
2021-02-24 12:10:42 +0100

    first commit

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Annonces;
+use App\Form\AnnoncesType;
+use App\Form\EditProfileType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UsersController extends AbstractController
+{
+    /**
+     * @Route("/users", name="users")
+     */
+    public function index(): Response
+    {
+        return $this->render('users/index.html.twig');
+    }
+
+     /**
+     * @Route("/users/annonces/ajout", name="users_annonces_ajout")
+     */
+    public function ajoutAnnonce(Request $request): Response
+    {
+
+        $annonce = new Annonces;
+
+        $form = $this->createForm(AnnoncesType::class, $annonce);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $annonce->setUsers($this->getUser());
+            $annonce->setActive(false);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($annonce);
+            $em->flush();
+
+            return $this->redirectToRoute('users');
+        }
+
+        return $this->render('users/annonces/ajout.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+     /**
+     * @Route("/users/profil/modifier", name="users_profil_modifier")
+     */
+    public function editProfile(Request $request): Response
+    {
+        $user =  $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('message', 'Profil mis à jour');
+            return $this->redirectToRoute('users');
+        }
+
+        return $this->render('users/editProfiletype.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+      /**
+     * @Route("/users/pass/modifier", name="users_pass_modifier")
+     */
+    public function editPass(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        if($request->isMethod('POST')){
+            $em = $this->getDoctrine()->getManager();
+
+            $user =  $this->getUser();
+
+            //on verifie si les 2 mots de pass sont identiques
+            if($request->request->get('pass') == $request->request->get('pass2')){
+                $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('pass')));
+                $em->flush();
+                $this->addFlash('message', 'mot de passe mis à jour avec succès');
+                return $this->redirectToRoute('users');
+            }else{
+                $this->addFlash('error', 'les 2 mots de passe ne sont pas identiques');
+            }
+        }
+        return $this->render('users/editpass.html.twig');
+    }
+}
---
+++ src/Form/AnnoncesType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonces;
+use App\Entity\Categories;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+
+class AnnoncesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('content', CKEditorType::class)
+            ->add('categories', EntityType::class, [
+                'class' => Categories::class
+            ])
+            ->add('Valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonces::class,
+        ]);
+    }
+}
